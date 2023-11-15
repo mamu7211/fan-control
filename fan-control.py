c9a3d65214a53f7fc997d252ee1b88e8680ee845
Martin <martin@murrer.io>
2023-11-15 13:51:20 +0100

    Fixes module error.

--- fan-control.py
+++ fan-control.py
@@ -22,7 +22,7 @@ try:
         elif fan.value > 0.0 and cpuTemp <= 35:
             syslog.syslog(syslog.LOG_INFO, f"Turning off fan, temp is {cpuTemp}°c.")
             fan.value = 0.0
-        if counter % 0 == 0:
+        if counter % 6 == 0:
             syslog.syslog(syslog.LOG_INFO, f"Current Temperature is {cpuTemp}°C.")
         sleep(10)
         counter += 1
